6ddc7c24c888e2ca804da27c393b50954063e00d
kudakiach <58485429+kudakiach@users.noreply.github.com>
2024-07-22 21:49:26 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,8 @@
 
 export const environment = {
   firebase: {
-    // projectId: 'my-wallet-system',
-    // appId: '1:539746537228:web:a40e8770d276ff42154329',
-    // storageBucket: 'my-wallet-system.appspot.com',
-    // apiKey: 'AIzaSyB-e1XgCoQtY5J4Fol8TZGO1Fb_F8Q--xc',
-    // authDomain: 'my-wallet-system.firebaseapp.com',
-    // messagingSenderId: '539746537228',
-    apiKey: "AIzaSyAfKLuIf4w38Vwhc2w0o6QH20qM2V8z-L8",
+    
+    apiKey: "*****",
   authDomain: "innovator-69232.firebaseapp.com",
   databaseURL: "https://innovator-69232-default-rtdb.firebaseio.com",
   projectId: "innovator-69232",
