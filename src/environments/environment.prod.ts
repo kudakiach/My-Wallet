81b89cd995abed0a70785eefc578d6bdb6a8f881
unknown <kiachkuda@gmail.com>
2024-07-22 15:02:28 +0300

    My Wallet aPP

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  firebase: {
+    // projectId: 'my-wallet-system',
+    // appId: '1:539746537228:web:a40e8770d276ff42154329',
+    // storageBucket: 'my-wallet-system.appspot.com',
+    // apiKey: 'AIzaSyB-e1XgCoQtY5J4Fol8TZGO1Fb_F8Q--xc',
+    // authDomain: 'my-wallet-system.firebaseapp.com',
+    // messagingSenderId: '539746537228',
+    apiKey: "AIzaSyAfKLuIf4w38Vwhc2w0o6QH20qM2V8z-L8",
+    authDomain: "innovator-69232.firebaseapp.com",
+    databaseURL: "https://innovator-69232-default-rtdb.firebaseio.com",
+    projectId: "innovator-69232",
+    storageBucket: "innovator-69232.appspot.com",
+    messagingSenderId: "545949567240",
+    appId: "1:545949567240:web:e45fb0e98e2f9b01aa44bd"
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,31 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    // projectId: 'my-wallet-system',
+    // appId: '1:539746537228:web:a40e8770d276ff42154329',
+    // storageBucket: 'my-wallet-system.appspot.com',
+    // apiKey: 'AIzaSyB-e1XgCoQtY5J4Fol8TZGO1Fb_F8Q--xc',
+    // authDomain: 'my-wallet-system.firebaseapp.com',
+    // messagingSenderId: '539746537228',
+    apiKey: "AIzaSyAfKLuIf4w38Vwhc2w0o6QH20qM2V8z-L8",
+  authDomain: "innovator-69232.firebaseapp.com",
+  databaseURL: "https://innovator-69232-default-rtdb.firebaseio.com",
+  projectId: "innovator-69232",
+  storageBucket: "innovator-69232.appspot.com",
+  messagingSenderId: "545949567240",
+  appId: "1:545949567240:web:e45fb0e98e2f9b01aa44bd"
+  },
+  production: true
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
